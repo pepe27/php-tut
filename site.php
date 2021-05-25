c86fe22a436b77c7e52c50d27007a15e7ec57d29
pepe27 <moimimixuan@gmail.com>
2021-05-25 18:22:47 -0400

    String/variable learned stuff

--- site.php
+++ site.php
@@ -10,7 +10,6 @@
       $characterName = "Bob";
       $characterAge = 90;
 
-
       echo "There once was a man named $characterName <br>";
       echo "He was $characterAge years old <br>";
       $characterName = "Mike";
@@ -22,8 +21,29 @@
       echo "<hr>";
       echo "<p>This is my site</p>";
 
+      $phrase ="To be or not to be";
+      $age = 30;
+      $gpa = 30.3; #there is a difference
+      $isMale = false;
+      null;
+      echo "Hello <br>\n"; #<br> makes line break in actual screen output
+      echo "\n"; #this only makes a line break in the elements box...
+      echo $phrase;
+      echo 30;
 
+      $phrase = "Giraffe Academy<br>";
+      echo strtoupper($phrase);
+      echo strtolower($phrase);
+      echo strlen($phrase); #amount of characters inside the string, includes the <br>
+      echo $phrase[0];
+      $phrase[0] = "B";
+      echo $phrase; #prints the phrase with the new $phrase[0] change
+      echo $phrase;
+      echo str_replace("Biraffe","Panda", $phrase); #Biraffe, due to $phrase changes on prior line
+      echo str_replace("ffe","Panda",$phrase);
 
+      echo substr($phrase, 8);
+      echo substr($phrase, 8,3);
 
   ?>
 
