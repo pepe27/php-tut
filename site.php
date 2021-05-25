9e46548fa251a6c36e6f6110b60e72e9b7e1ea8e
pepe27 <moimimixuan@gmail.com>
2021-05-25 17:20:34 -0400

    initial commit

---
+++ site.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+      $characterName = "Bob";
+      $characterAge = 90;
+
+
+      echo "There once was a man named $characterName <br>";
+      echo "He was $characterAge years old <br>";
+      echo "He didn't like the name $characterName<br>";
+      echo "But he did like being $characterAge<br>";
+
+      echo("Hello World"); #dont need brackets
+      echo "<h1>Mimi's Site </h1>";
+      echo "<hr>";
+      echo "<p>This is my site</p>";
+
+
+
+
+  ?>
+
+
+
+
+  </body>
+</html>
